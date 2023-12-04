6807e64c557f63de9aeed1463d8d2b6218c54790
Sayman Rifat <saymanrifat@gmail.com>
2023-12-04 14:24:40 +0600

    Home Screen Update

--- lib/view/screen/home_page.dart
+++ lib/view/screen/home_page.dart
@@ -14,22 +14,18 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Colors.blue,
-        title: const Text("Social Media"),
-      ),
-      body: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        shrinkWrap: true,
-        itemCount: 10,
-          itemBuilder: (context,index){
-          if(index == 0){
-            return const MyProfileStory();
-          }
-        return const OtherProfileStory();
-      })
-    );
+        body: SafeArea(
+      child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          shrinkWrap: true,
+          itemCount: 10,
+          itemBuilder: (context, index) {
+            if (index == 0) {
+              return const MyProfileStory();
+            }
+            return const OtherProfileStory();
+          }),
+    ));
   }
 }
 
