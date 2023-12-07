77be346a3d9ade43d5fbe8216bf7d81b00378722
Sayman Rifat <saymanrifat@gmail.com>
2023-12-07 21:30:25 +0600

    Add Search Screen & Upload User Screen with some image url(tamp)

--- lib/view/widget/image_data.dart
+++ lib/view/widget/image_data.dart
@@ -5,30 +5,54 @@ class ImageData {
 
 
 List<ImageData> getImageDataList() {
-return [
-ImageData(
-'https://images.tokopedia.net/img/cache/250-square/VqbcmM/2023/1/4/816e2718-4943-41a9-b55b-b0bc48311a42.jpg'),
-ImageData(
-'https://w0.peakpx.com/wallpaper/609/20/HD-wallpaper-black-girl-illustration-black-love-art-black-girl-art-black-art-painting-african-women-art.jpg'),
-ImageData(
-  'https://icsblog.s3.ap-south-1.amazonaws.com/blog/wp-content/uploads/2023/07/20215423/10001000p507EDNmainimg-motivation-800x4501-1.jpg'),
-ImageData(
-'https://imgsrv2.voi.id/NCMxBshCs2v0reDTvD27OMf3nE1cAPhzW-RynvJgVjs/auto/1200/675/sm/1/bG9jYWw6Ly8vcHVibGlzaGVycy8xMTE2NTQvMjAyMTEyMDcyMDAyLW1haW4uY3JvcHBlZF8xNjM4ODgyMjQ3LmpwZw.jpg'),
-ImageData(
-'https://cdn.elearningindustry.com/wp-content/uploads/2022/06/shutterstock_2114935928.jpg'),
-ImageData(
-'https://hips.hearstapps.com/hmg-prod/images/short-inspirational-quotes-lead-1597431860.jpg?crop=1xw:0.8428772304111715xh;center,top'),
-ImageData(
-'https://www.invajy.com/wp-content/uploads/2019/04/Deep-Quotes-1.jpg'),
-ImageData(
-'https://i.pinimg.com/736x/a4/7e/54/a47e54f348af8a8d17afa632c89cc5de.jpg'),
-ImageData(
-'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_39dRdsOAMnYiSTU8q4LmziH_eIzgPxTJRFTnM7gb4Z6Ue4R4RM1p21ExXwz6UnuHDGA&usqp=CAU'),
-ImageData(
-'https://i.pinimg.com/736x/26/c9/1f/26c91f4e94f67dc3f51d848c98482c89.jpg'),
-ImageData(
-'https://www.penguins.co.uk/hubfs/employee_motivation_header.jpg'),
-ImageData(
-'https://i.ytimg.com/vi/56Qu3MY5EnU/maxresdefault.jpg'),
-];
+  return [
+    ImageData(
+        'https://images.tokopedia.net/img/cache/250-square/VqbcmM/2023/1/4/816e2718-4943-41a9-b55b-b0bc48311a42.jpg'),
+    ImageData(
+        'https://w0.peakpx.com/wallpaper/609/20/HD-wallpaper-black-girl-illustration-black-love-art-black-girl-art-black-art-painting-african-women-art.jpg'),
+    ImageData(
+        'https://icsblog.s3.ap-south-1.amazonaws.com/blog/wp-content/uploads/2023/07/20215423/10001000p507EDNmainimg-motivation-800x4501-1.jpg'),
+    ImageData(
+        'https://imgsrv2.voi.id/NCMxBshCs2v0reDTvD27OMf3nE1cAPhzW-RynvJgVjs/auto/1200/675/sm/1/bG9jYWw6Ly8vcHVibGlzaGVycy8xMTE2NTQvMjAyMTEyMDcyMDAyLW1haW4uY3JvcHBlZF8xNjM4ODgyMjQ3LmpwZw.jpg'),
+    ImageData(
+        'https://cdn.elearningindustry.com/wp-content/uploads/2022/06/shutterstock_2114935928.jpg'),
+    ImageData(
+        'https://hips.hearstapps.com/hmg-prod/images/short-inspirational-quotes-lead-1597431860.jpg?crop=1xw:0.8428772304111715xh;center,top'),
+    ImageData(
+        'https://www.invajy.com/wp-content/uploads/2019/04/Deep-Quotes-1.jpg'),
+    ImageData(
+        'https://i.pinimg.com/736x/a4/7e/54/a47e54f348af8a8d17afa632c89cc5de.jpg'),
+    ImageData(
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_39dRdsOAMnYiSTU8q4LmziH_eIzgPxTJRFTnM7gb4Z6Ue4R4RM1p21ExXwz6UnuHDGA&usqp=CAU'),
+    ImageData(
+        'https://i.pinimg.com/736x/26/c9/1f/26c91f4e94f67dc3f51d848c98482c89.jpg'),
+    ImageData(
+        'https://www.penguins.co.uk/hubfs/employee_motivation_header.jpg'),
+    ImageData(
+        'https://i.ytimg.com/vi/56Qu3MY5EnU/maxresdefault.jpg'),
+    ImageData(
+        'https://images.tokopedia.net/img/cache/250-square/VqbcmM/2023/1/4/816e2718-4943-41a9-b55b-b0bc48311a42.jpg'),
+    ImageData(
+        'https://w0.peakpx.com/wallpaper/609/20/HD-wallpaper-black-girl-illustration-black-love-art-black-girl-art-black-art-painting-african-women-art.jpg'),
+    ImageData(
+        'https://icsblog.s3.ap-south-1.amazonaws.com/blog/wp-content/uploads/2023/07/20215423/10001000p507EDNmainimg-motivation-800x4501-1.jpg'),
+    ImageData(
+        'https://imgsrv2.voi.id/NCMxBshCs2v0reDTvD27OMf3nE1cAPhzW-RynvJgVjs/auto/1200/675/sm/1/bG9jYWw6Ly8vcHVibGlzaGVycy8xMTE2NTQvMjAyMTEyMDcyMDAyLW1haW4uY3JvcHBlZF8xNjM4ODgyMjQ3LmpwZw.jpg'),
+    ImageData(
+        'https://cdn.elearningindustry.com/wp-content/uploads/2022/06/shutterstock_2114935928.jpg'),
+    ImageData(
+        'https://hips.hearstapps.com/hmg-prod/images/short-inspirational-quotes-lead-1597431860.jpg?crop=1xw:0.8428772304111715xh;center,top'),
+    ImageData(
+        'https://www.invajy.com/wp-content/uploads/2019/04/Deep-Quotes-1.jpg'),
+    ImageData(
+        'https://i.pinimg.com/736x/a4/7e/54/a47e54f348af8a8d17afa632c89cc5de.jpg'),
+    ImageData(
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_39dRdsOAMnYiSTU8q4LmziH_eIzgPxTJRFTnM7gb4Z6Ue4R4RM1p21ExXwz6UnuHDGA&usqp=CAU'),
+    ImageData(
+        'https://i.pinimg.com/736x/26/c9/1f/26c91f4e94f67dc3f51d848c98482c89.jpg'),
+    ImageData(
+        'https://www.penguins.co.uk/hubfs/employee_motivation_header.jpg'),
+    ImageData(
+        'https://i.ytimg.com/vi/56Qu3MY5EnU/maxresdefault.jpg'),
+  ];
 }
