60f847dc7863b30327d43316f066d753651fac6e
Moniruzzaman <107347380+moniruzzaman76@users.noreply.github.com>
2023-12-12 13:24:04 +0600

    home screen has been done with bottom Nab Bar using Getx

--- lib/view/screen/main_bottom_navigation_screen.dart
+++ lib/view/screen/main_bottom_navigation_screen.dart
@@ -1,6 +1,7 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/src/simple/get_state.dart';
 import 'package:iconsax/iconsax.dart';
+import '../../state_holders/main_bottom_nav_controller.dart';
 import '../../utils/colors.dart';
 import 'home_screen.dart';
 import 'my_profile_screen.dart';
@@ -22,40 +23,39 @@ class _MainBottomNavigationScreenState extends State<MainBottomNavigationScreen>
 
   ];
 
-  int _index=0;
-
   @override
   Widget build(BuildContext context) {
     Theme.of(context).brightness;
-    return Scaffold(
-      body: _screens[_index],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _index,
-        onTap: (value) {
-          print(value);
-           _index =value;
-           setState(() {
-           });
-        },
-        backgroundColor: Theme.of(context).brightness == Brightness.dark ?
-        AppColors.darkBackgroundColor : AppColors.lightBackgroundColor,
-        selectedItemColor: AppColors.primaryColor,
-        unselectedItemColor: Theme.of(context).brightness == Brightness.dark ?
-        AppColors.lightBackgroundColor : AppColors.darkBackgroundColor,
-        showUnselectedLabels: false,
-        elevation: 0,
-        type: BottomNavigationBarType.fixed,
-        items: const [
-          BottomNavigationBarItem(
-              icon: Icon(Iconsax.home,), label: ""),
-          BottomNavigationBarItem(
-              icon: Icon(Iconsax.search_normal_1,), label: ""),
-          BottomNavigationBarItem(
-              icon: Icon(Iconsax.add,), label: ""),
-          BottomNavigationBarItem(
-              icon: Icon(Iconsax.profile_circle,), label: ""),
-        ],
-      ),
+    return GetBuilder<MainBottomNavController>(
+        builder: (controller) {
+          return Scaffold(
+            body: _screens[controller.selectedIndex],
+            bottomNavigationBar: BottomNavigationBar(
+              currentIndex: controller.selectedIndex,
+              onTap: controller.changeScreen,
+
+              backgroundColor: Theme.of(context).brightness == Brightness.dark ?
+              AppColors.darkBackgroundColor : AppColors.lightBackgroundColor,
+              selectedItemColor: AppColors.primaryColor,
+              unselectedItemColor: Theme.of(context).brightness == Brightness.dark ?
+              AppColors.lightBackgroundColor : AppColors.darkBackgroundColor,
+              showUnselectedLabels: false,
+              elevation: 0,
+              // type: BottomNavigationBarType.fixed,
+
+              items: const [
+                BottomNavigationBarItem(
+                    icon: Icon(Iconsax.home,), label: ""),
+                BottomNavigationBarItem(
+                    icon: Icon(Iconsax.search_normal_1,), label: ""),
+                BottomNavigationBarItem(
+                    icon: Icon(Iconsax.add,), label: ""),
+                BottomNavigationBarItem(
+                    icon: Icon(Iconsax.profile_circle,), label: ""),
+              ],
+            ),
+          );
+        }
     );
   }
 }
